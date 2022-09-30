68d7da3165f6413f17dd5fb5a51d45266043ab0a
Lovro Kunović <lovro_kunovic@hotmail.com>
2022-09-30 19:02:34 +0200

    Add classes, install binaries

--- entryPoint.py
+++ entryPoint.py
@@ -1,20 +1,32 @@
 import csv
+import pandas as pd
+import re
 
-list = []
-dict = {}
+class Person:
+    def __init__(self, id, survived, sex):
+        self.id = id
+        self.survived = survived
+        self.sex = sex
+
+    def __str__(self):
+        return f"{self.id},{self.survived},{self.sex}"
+
+people = []
 with open('./data/titanic/train.csv', "r") as file:
     reader = csv.reader(file, delimiter="\t")
     for i, line in enumerate(reader):
         if i == 0 :
             continue
-        # print 'line[{}] = {}'.format(i, line)
-        list.append(line)
-        print line
-        stripped = line
-        print stripped
-
-        print ''
-        dict[i] = line
 
 
-# print dict
+        # list to string conversion
+        stripped = line[0]
+        stripped = re.split(r',(?=[^\s]+)',stripped)
+        print(stripped)
+        person = Person(stripped[0], stripped[1], stripped[4])
+       
+        people.append(person)
+
+
+# data = pd.read_csv('./data/titanic/train.csv')
+# print(data.to_string())
