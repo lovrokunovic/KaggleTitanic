c20326f437226da4197d66eeb6bfc027edfb37c3
Lovro Kunović <lovro_kunovic@hotmail.com>
2022-09-30 21:55:22 +0200

    Basic calculation, python exercise

--- entryPoint.py
+++ entryPoint.py
@@ -9,7 +9,7 @@ class Person:
         self.gender = gender
 
     def __str__(self):
-        return f"{self.id},{self.survived},{self.gender}"
+        return f"id:{self.id}, survived: {self.survived}, gender: {self.gender}"
 
 people = []
 with open('./data/titanic/train.csv', "r") as file:
@@ -22,11 +22,34 @@ with open('./data/titanic/train.csv', "r") as file:
         # list to string conversion
         stripped = line[0]
         stripped = re.split(r',(?=[^\s]+)',stripped)
-        print(stripped)
-        person = Person(stripped[0], stripped[1], stripped[4])
-       
+        # print(stripped)
+        person = Person(stripped[0], int(stripped[1]), stripped[4] )
         people.append(person)
 
-print(people[62])
+print()
 # data = pd.read_csv('./data/titanic/train.csv')
 # print(data.to_string())
+total = 0
+survived = 0
+males = 0
+females = 0
+males_survived_count = 0
+females_survived_count = 0
+
+for p in people:
+    total += 1
+    if(p.gender == "male"):
+        males += 1
+        if(p.survived):
+            survived += 1
+            males_survived_count += 1
+    if(p.gender == "female"):
+        females += 1
+        if(p.survived):
+            survived += 1
+            females_survived_count += 1
+
+print("survived:",survived, "out of", total, "total,", round((survived/total)*100, 2), "%")
+print("males survived:", males_survived_count, "out of", males, "total,", round((males_survived_count/males)*100, 2), "%")
+print("females survived:", females_survived_count, "out of", females, "total,", round((females_survived_count/females)*100, 2), "%") 
+print()
