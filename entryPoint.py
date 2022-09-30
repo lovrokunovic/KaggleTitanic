0cf1d4eafd50223a090b47fc92ab892bf4469d66
Lovro Kunović <lovro_kunovic@hotmail.com>
2022-09-30 08:49:16 +0200

    Basic line reading

--- entryPoint.py
+++ entryPoint.py
@@ -1,2 +1,20 @@
+import csv
 
-print 'Hello World!'
+list = []
+dict = {}
+with open('./data/titanic/train.csv', "r") as file:
+    reader = csv.reader(file, delimiter="\t")
+    for i, line in enumerate(reader):
+        if i == 0 :
+            continue
+        # print 'line[{}] = {}'.format(i, line)
+        list.append(line)
+        print line
+        stripped = line
+        print stripped
+
+        print ''
+        dict[i] = line
+
+
+# print dict
