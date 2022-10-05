c51bbfcd06247f5ca562a12bbb972558d1f96516
Lovro Kunović <lovro_kunovic@hotmail.com>
2022-10-05 08:55:36 +0200

    Update

--- entryPoint.py
+++ entryPoint.py
@@ -11,49 +11,76 @@ class Person:
     def __str__(self):
         return f"id:{self.id}, survived: {self.survived}, gender: {self.gender}"
 
+
 people = []
+survived = []
+not_survived = []
+
 with open('./data/titanic/train.csv', "r") as file:
     reader = csv.reader(file, delimiter="\t")
     for i, line in enumerate(reader):
         if i == 0 :
             continue
-
-
         # list to string conversion
         stripped = line[0]
-        stripped = re.split(r',(?=[^\s]+)',stripped)
-        # print(stripped)
-        person = Person(stripped[0], int(stripped[1]), stripped[4] )
+        stripped = re.split(r',(?=[^\s]+)', stripped)
+        person = Person(stripped[0], int(stripped[1]), stripped[4])
+       
         people.append(person)
+        if(person.survived):
+            survived.append(person)
+        else:
+            not_survived.append(person)
+        
 
 print()
-total = 0
-survived = 0
-males = 0
-females = 0
-males_survived_count = 0
-females_survived_count = 0
+total = len(survived) + len(not_survived) # = i
 
-for p in people:
-    total += 1
-    if(p.gender == "male"):
-        males += 1
-        if(p.survived):
-            survived += 1
-            males_survived_count += 1
-    if(p.gender == "female"):
-        females += 1
-        if(p.survived):
-            survived += 1
-            females_survived_count += 1
-
-print("survived:",survived, "out of", total, "total,", round((survived/total)*100, 2), "%")
-print("males survived:", males_survived_count, "out of", males, "total,", round((males_survived_count/males)*100, 2), "%")
-print("females survived:", females_survived_count, "out of", females, "total,", round((females_survived_count/females)*100, 2), "%") 
+print("survived:",len(survived), "out of", total, "total,", round((len(survived)/total)*100, 2), "%")
+   
+       
+print()
+print()
+print()
 print()
 
 
+data_frame = pandas.read_csv('./data/titanic/train.csv')
+# print("rows, cols:", data_frame.shape) 
+# print(data_frame.info())
+data_frame = data_frame[["Survived", "Pclass", "Sex"]]
+result = data_frame.groupby(['Survived'])
 
-data_frames = pandas.read_csv('./data/titanic/train.csv')
-print("rows, cols:", data_frames.shape) 
-print(data_frames.info())
+survived = pandas.DataFrame()
+not_survived = pandas.DataFrame()
+for survived, group in result:
+    if(survived == 0):
+        not_survived = group
+    elif(survived == 1):
+        survived = group
+
+print(survived)
+print()
+print(not_survived)
+
+apriori_prob_survived = len(survived)/float(len(data_frame))
+apriori_prob_not_survived = len(not_survived)/float(len(data_frame))
+
+pclass_counts_not_survived = not_survived['Pclass'].value_counts()
+pclass_counts_survived = survived['Pclass'].value_counts()
+
+sex_counts_not_survived = not_survived['Sex'].value_counts()
+sex_counts_survived = survived['Sex'].value_counts()
+
+print()
+print("Not survived:")
+print(pclass_counts_not_survived)
+print(sex_counts_not_survived)
+
+print()
+print("Survived:")
+print(pclass_counts_survived)
+print(sex_counts_survived)
+print()
+
+print(dict(sex_counts_survived))
