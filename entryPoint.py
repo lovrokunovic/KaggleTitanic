29c81a547fcd3fbdf4a672a22aff000e258afc62
Lovro Kunović <lovro_kunovic@hotmail.com>
2022-10-05 18:46:41 +0200

    Remove old code

--- entryPoint.py
+++ entryPoint.py
@@ -2,47 +2,6 @@ import csv
 import pandas 
 import re
 
-class Person:
-    def __init__(self, id, survived, gender):
-        self.id = id
-        self.survived = survived
-        self.gender = gender
-
-    def __str__(self):
-        return f"id:{self.id}, survived: {self.survived}, gender: {self.gender}"
-
-
-people = []
-survived = []
-not_survived = []
-
-with open('./data/titanic/train.csv', "r") as file:
-    reader = csv.reader(file, delimiter="\t")
-    for i, line in enumerate(reader):
-        if i == 0 :
-            continue
-        # list to string conversion
-        stripped = line[0]
-        stripped = re.split(r',(?=[^\s]+)', stripped)
-        person = Person(stripped[0], int(stripped[1]), stripped[4])
-       
-        people.append(person)
-        if(person.survived):
-            survived.append(person)
-        else:
-            not_survived.append(person)
-        
-
-print()
-total = len(survived) + len(not_survived) # = i
-
-print("survived:",len(survived), "out of", total, "total,", round((len(survived)/total)*100, 2), "%")
-   
-       
-print()
-print()
-print()
-print()
 
 
 data_frame = pandas.read_csv('./data/titanic/train.csv')
